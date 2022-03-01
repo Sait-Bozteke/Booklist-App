92108bfc7e6f000397417e559c0e33a43099b61c
sait <sait.bozteke63@gmail.com>
2022-03-01 23:15:23 +0100

    file added

---
+++ app.js
@@ -0,0 +1,31 @@
+class Book{
+constructor(title, author, isbn){
+    this.title=title;
+    this.author=author;
+    this.isbn=isbn;
+}
+
+
+}
+
+class UI{
+static displayBooks(){
+    const StoredBooks={
+
+        title:"Book one",
+        author:"jane Doe",
+        isbn:"45545"
+    }
+    {
+        title:"Book Two",
+        author:"jane Doe",
+        isbn:"45545"
+
+    }
+    const books=StoredBooks
+}
+
+
+
+
+}
